8b47db42bae4c0845a3fd56cf518a92772ac7014
RyanIsAinmDom <96996608+RyanIsAinmDom@users.noreply.github.com>
2025-09-01 21:15:06 -0400

    Update LrHuskTowns.java
    Fix hook for v3

--- Core/src/main/java/biz/donvi/jakesRTP/claimsIntegrations/LrHuskTowns.java
+++ Core/src/main/java/biz/donvi/jakesRTP/claimsIntegrations/LrHuskTowns.java
@@ -1,12 +1,13 @@
 package biz.donvi.jakesRTP.claimsIntegrations;
 
-import me.william278.husktowns.HuskTownsAPI;
+import net.william278.husktowns.api.HuskTownsAPI;
+import net.william278.husktowns.api.BukkitHuskTownsAPI;
 import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 
 public class LrHuskTowns implements LocationRestrictor {
 
-    protected Plugin cmPlugin;
+    protected final Plugin cmPlugin;
 
     public LrHuskTowns(Plugin cmPlugin) {
         this.cmPlugin = cmPlugin;
@@ -19,6 +20,13 @@ public class LrHuskTowns implements LocationRestrictor {
 
     @Override
     public boolean denyLandingAtLocation(Location location) {
-        return (!HuskTownsAPI.getInstance().isWilderness(location));
+        // Get the API instance, then use Bukkit extensions if available
+        final HuskTownsAPI base = HuskTownsAPI.getInstance(); // v3 entrypoint
+        if (base instanceof BukkitHuskTownsAPI api) {
+            // On Bukkit, adapt org.bukkit.Location -> Position and check if claimed
+            return api.isClaimAt(api.getPosition(location));
+        }
+        // If somehow not on Bukkit API (shouldn't happen on Paper/Spigot), allow landing
+        return false;
     }
 }
