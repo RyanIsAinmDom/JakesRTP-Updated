7b89672ba3b58a035d1c295388040d36f0a06b5f
RyanIsAinmDom <96996608+RyanIsAinmDom@users.noreply.github.com>
2025-09-03 04:32:31 -0400

    Fix HuskTowns V3 Hook

--- Core/src/main/java/biz/donvi/jakesRTP/claimsIntegrations/LrHuskTowns.java
+++ Core/src/main/java/biz/donvi/jakesRTP/claimsIntegrations/LrHuskTowns.java
@@ -2,12 +2,13 @@ package biz.donvi.jakesRTP.claimsIntegrations;
 
 import net.william278.husktowns.api.HuskTownsAPI;
 import net.william278.husktowns.api.BukkitHuskTownsAPI;
+import net.william278.husktowns.claim.Position;
 import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 
 public class LrHuskTowns implements LocationRestrictor {
 
-    protected final Plugin cmPlugin;
+    protected Plugin cmPlugin;
 
     public LrHuskTowns(Plugin cmPlugin) {
         this.cmPlugin = cmPlugin;
@@ -20,13 +21,8 @@ public class LrHuskTowns implements LocationRestrictor {
 
     @Override
     public boolean denyLandingAtLocation(Location location) {
-        // Get the API instance, then use Bukkit extensions if available
-        final HuskTownsAPI base = HuskTownsAPI.getInstance(); // v3 entrypoint
-        if (base instanceof BukkitHuskTownsAPI api) {
-            // On Bukkit, adapt org.bukkit.Location -> Position and check if claimed
-            return api.isClaimAt(api.getPosition(location));
-        }
-        // Should never happen but makes compiler happy and therefore me
-        return false;
+        // Convert Bukkit Location to HuskTowns Position and check if it's claimed (not wilderness)
+        Position position = BukkitHuskTownsAPI.getInstance().getPosition(location);
+        return position != null && HuskTownsAPI.getInstance().isClaimAt(position);
     }
 }
