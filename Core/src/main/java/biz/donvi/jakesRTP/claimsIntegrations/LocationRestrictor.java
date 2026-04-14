570616ffb0c6ee05161b13171e33694225993c68
RyanIsAinmDom <96996608+RyanIsAinmDom@users.noreply.github.com>
2026-04-14 17:54:00 -0400

    Update LocationRestrictor.java
    Remove redundant public modifiers

--- Core/src/main/java/biz/donvi/jakesRTP/claimsIntegrations/LocationRestrictor.java
+++ Core/src/main/java/biz/donvi/jakesRTP/claimsIntegrations/LocationRestrictor.java
@@ -1,12 +1,10 @@
 package biz.donvi.jakesRTP.claimsIntegrations;
-
 import org.bukkit.Location;
 import org.bukkit.plugin.Plugin;
 
 public interface LocationRestrictor {
-
-    public Plugin supporterPlugin();
-
-    public boolean denyLandingAtLocation(Location location);
-
+    
+    Plugin supporterPlugin();
+    
+    boolean denyLandingAtLocation(Location location);
 }
