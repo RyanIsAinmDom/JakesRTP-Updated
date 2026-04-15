928a54a52b1e27dbf92482bbe724b8ec5e88769b
RyanIsAinmDom <96996608+RyanIsAinmDom@users.noreply.github.com>
2026-04-15 10:38:52 -0400

    Update SafeLocationFinderBukkitThread.java
    Added warning about sync chunk load

--- Core/src/main/java/biz/donvi/jakesRTP/SafeLocationFinderBukkitThread.java
+++ Core/src/main/java/biz/donvi/jakesRTP/SafeLocationFinderBukkitThread.java
@@ -3,6 +3,8 @@ package biz.donvi.jakesRTP;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
+import java.util.logging.Level;
+
 import static biz.donvi.jakesRTP.SafeLocationUtils.requireMainThread;
 
 public class SafeLocationFinderBukkitThread extends SafeLocationFinder {
@@ -17,7 +19,12 @@ public class SafeLocationFinderBukkitThread extends SafeLocationFinder {
     @Override
     protected Material getLocMaterial(Location loc) {
         requireMainThread();
-        loc.getChunk().load();        //Sync chunk load; this is a fallback method and should only be needed in extremely rare scenarios (if ever). Otherwise, chunk loading is always async.
+        if (!loc.getChunk().isLoaded()) {
+            JakesRtpPlugin.log(Level.WARNING,
+                "[JRTP] Synchronous chunk load triggered in SafeLocationFinderBukkitThread. " + //This happens when a chunk refuses to be loaded asyncronously. At this point, the chunk must be loaded by any means necessary.
+                "This may cause a server stutter. If this warning is frequent, please report it.");
+            loc.getChunk().load(); //Sync chunk load; this is a fallback method and should only be needed in extremely rare scenarios (if ever). Otherwise, chunk loading is always async.
+        }
         return loc.getBlock().getType();
     }
 
